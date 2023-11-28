a10d7d0abaa810f06f8eacfd5c685b2779e45b9e
Joel Nicolow <jnicolow@hawaii.edu>
2023-11-27 14:26:00 -1000

    line chart

--- streamlitspiderbaruserselect.py
+++ streamlitspiderbaruserselect.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import pandas as pd
 import numpy as np
 from glob import glob
@@ -38,13 +39,24 @@ def main():
             fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
             ax.set_title(f'Hourly fog frequency for {site_selected}')
             plt.xticks(angles[:-1], series.index, color='grey', size=8)
-            ax.set_theta_offset(np.pi/2) # Rotate the plot by 90 degrees
-            ax.set_theta_direction(-1) # Make the plot go clockwise
+            ax.set_theta_offset(np.pi/2) # rotate the plot by 90 degrees so 0 oclock is at the top
+            ax.set_theta_direction(-1) # make the plot go clockwise
             ax.set_rlabel_position(30)
             bars = ax.bar(angles[:-1], values[:-1], width=0.35, alpha=0.9, linewidth=2, edgecolor='none')
-            ax.grid(False) # This line removes the grid
+            ax.grid(False) # remove grid lines
             ax.set_yticklabels([])
             st.pyplot(fig)
+
+        df['date'] = df['timestamp'].dt.date
+        daily_frequencies_total = df.groupby('date').size()
+        daily_frequencies_category1 = df[df['category'] == 1].groupby('date').size()
+        daily_percentages_category1 = (daily_frequencies_category1 / daily_frequencies_total) * 100
+        fig, ax = plt.subplots()
+        ax.plot(daily_percentages_category1.index, daily_percentages_category1.values)
+        ax.xaxis.set_major_locator(mdates.AutoDateLocator())
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y.%m.%d'))
+        fig.autofmt_xdate()
+        st.pyplot(fig)
     else:
         st.experimental_rerun()
 
