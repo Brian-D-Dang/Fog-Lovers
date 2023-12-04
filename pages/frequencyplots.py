bef69d22aff7271c006aea269c49205028e9b3b7
Joel Nicolow <jnicolow@hawaii.edu>
2023-12-04 07:45:38 -1000

    adding legend

--- pages/frequencyplots.py
+++ pages/frequencyplots.py
@@ -39,6 +39,9 @@ def main():
         siteNames.append(firstSplit[-1].split('.csv')[0].split('_')[0])
     sites_selected = st.sidebar.multiselect("Select sites", siteNames)
 
+   # Define the figure and axes outside the loop
+    fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
+
     for site_selected in sites_selected:
         file_selected = None
         for data_file in data_files:
@@ -47,10 +50,12 @@ def main():
 
         if not file_selected is None:
             df = pd.read_csv(file_selected)
+            print(file_selected)
             df = df[df['category'] != -999]
             df['timestamp'] = pd.to_datetime(df['timestamp'])
             df['hour'] = df['timestamp'].dt.hour
             df_category1 = df[df['category'] == 1]
+            ### barchart
             hourly_frequencies_category1 = df_category1.groupby('hour').size()
             hourly_frequencies_total = df.groupby('hour').size()
             series = hourly_frequencies_category1 / hourly_frequencies_total
@@ -62,13 +67,16 @@ def main():
                 values += values[:1] # repeat the first value to close the circle
                 angles += angles[:1]
 
-                fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
+                # Set the title for each subplot
                 ax.set_title(f'Hourly fog frequency for {site_selected}')
                 plt.xticks(angles[:-1], series.index, color='grey', size=8)
                 ax.set_theta_offset(np.pi/2) # rotate the plot by 90 degrees so 0 oclock is at the top
                 ax.set_theta_direction(-1) # make the plot go clockwise
                 ax.set_rlabel_position(30)
                 bars = ax.bar(angles[:-1], values[:-1], width=0.15, alpha=0.9, linewidth=2, edgecolor='none') # reduce the width of the bars
+                for bar in bars:  # loop through each bar in the bar plot
+                    bar.set_label(site_selected)  # set the label for each bar
+                    break
                 ax.grid(False) # remove grid lines
                 ax.set_yticklabels([])
 
@@ -78,34 +86,32 @@ def main():
                 ax.set_yticklabels([f'{tick:.2f}' for tick in r_ticks], color='grey', size=8)  # Format the labels as you wish
                 ax.yaxis.grid(True)  # Add grid lines for the y-axis
 
-                st.pyplot(fig)
-
-            df['date'] = df['timestamp'].dt.date
-            daily_frequencies_total = df.groupby('date').size()
-            daily_frequencies_category1 = df[df['category'] == 1].groupby('date').size()
-            daily_percentages_category1 = (daily_frequencies_category1 / daily_frequencies_total) * 100
-
-            # Create a date range that includes all dates in your data
-            date_range = pd.date_range(start=df['date'].min(), end=df['date'].max())
-
-            # Convert your data to a Series with the date range as the index
-            daily_percentages_category1 = pd.Series(daily_percentages_category1, index=date_range)
-
-            fig, ax = plt.subplots()
-            ax.plot(daily_percentages_category1.index, daily_percentages_category1.values)
-
-            # Set the major ticks to the first day of each month.
-            ax.xaxis.set_major_locator(mdates.MonthLocator())
-
-            # Set the major tick labels to the month and year.
-            ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-
-            fig.autofmt_xdate()
-            st.pyplot(fig)
 
 
-        else:
-            st.experimental_rerun()
+    # Show the plot after the loop
+    plt.legend(loc='upper right')
+    st.pyplot(fig)
+
+
+
+        #     # line chart
+        #     df['date'] = df['timestamp'].dt.date
+        #     daily_frequencies_total = df.groupby('date').size()
+        #     daily_frequencies_category1 = df[df['category'] == 1].groupby('date').size()
+        #     daily_percentages_category1 = (daily_frequencies_category1 / daily_frequencies_total) * 100
+        #     date_range = pd.date_range(start=df['date'].min(), end=df['date'].max())
+        #     daily_percentages_category1 = pd.Series(daily_percentages_category1, index=date_range)
+        #     # fig2, ax2 = plt.subplots()
+        #     ax2.plot(daily_percentages_category1.index, daily_percentages_category1.values)
+        #     ax2.xaxis.set_major_locator(mdates.MonthLocator())
+        #     ax2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+
+        #     fig2.autofmt_xdate()
+        #     st.pyplot(fig2)
+        
+        # else:
+        #     st.experimental_rerun()
+
 
 if __name__ == "__main__":
     main()
